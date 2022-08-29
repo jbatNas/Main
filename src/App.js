c69a7db4fd6f265d308ccdf88a5db10ad16ddc39
jbatNas <jebatyani4004@gmail.com>
2022-08-29 15:55:48 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ let navigate = useNavigate();
     <div className="App">
      <p className='font-bold text-3xl text-center'>Project Penjana</p>
      <p>2022</p>
+     <p>KaKiKu</p>
      <p>By: JbatYani</p>
      <button className='border-2' onClick = {() => navigate('/landingpage')}>Go to landing page</button>
      <div></div>
